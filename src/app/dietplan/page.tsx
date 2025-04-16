7146bb59d27a0adb9e04ce1dc51ad24e186dd7c4
Alper Ceyhun <80386511+AlperCeyhun@users.noreply.github.com>
2025-04-16 13:34:00 +0300

    created dummy dietplan page

---
+++ src/app/dietplan/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+import React from "react";
+
+export default function Home() {
+  
+    return (
+      <div className="flex justify-center items-center min-h-screen flex-col">
+        
+      </div>
+    );
+  }
+  
