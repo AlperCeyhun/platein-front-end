6dd5c578fcc67364332ee89b3fa82df122e9f2a1
Alper Ceyhun <80386511+AlperCeyhun@users.noreply.github.com>
2025-05-04 02:28:16 +0300

    added healthConcern and activityLevel to account page

--- src/testdata/TestAccountData.tsx
+++ src/testdata/TestAccountData.tsx
@@ -12,22 +12,9 @@ const TestAccountData = [
     dailyMeals: 3,
     sleepingpattern: "-5 Hours",
     dailyWaterIntake: "4-6 glasses",
-    eatingStyle: "Vegan"
+    eatingStyle: "Vegan",
+    healthConcern: "Diabetes",
+    activityLevel: "Sedentary",
   },
-  {
-    id: 2,
-    name: "Cool",
-    surname: "Fellow",
-    email: "coolFellow@hotmail.com",
-    isMale: false,
-    password: "markHerword",
-    age: 21,
-    weight: 69,
-    goalweight: 65,
-    dailyMeals: 2,
-    sleepingpattern: "6 Hours",
-    dailyWaterIntake: "4-6 glasses",
-    eatingStyle: "anything"
-  }
 ];
 export default TestAccountData;
